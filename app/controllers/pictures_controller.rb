66acc5b977e33875960425d022e428e9bdb4d185
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-10-06 18:18:21 -0400

    Added individual views

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,44 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = [{
+      :title => "Bob",
+      :artist => "Billy Bob",
+      :url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+    },
+    {
+      :title => "Ned",
+      :artist => "Flanders",
+      :url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+    },
+    {
+      :title => "Homer",
+      :artist => "Simpson",
+      :url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+    }
+  ]
+  end
 
+  def picture0
+    @picture = {
+      :title => "Bob",
+      :artist => "Billy Bob",
+      :url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+    }
+  end
+
+  def picture1
+    @picture = {
+      :title => "Ned",
+      :artist => "Flanders",
+      :url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+    }
+  end
+
+  def picture2
+    @picture = {
+      :title => "Homer",
+      :artist => "Simspon",
+      :url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
 
  get "pictures" => "pictures#index"
+ get "pictures/0" => "pictures#picture0"
+ get "pictures/1" => "pictures#picture1"
+ get "pictures/2" => "pictures#picture2"
 end
